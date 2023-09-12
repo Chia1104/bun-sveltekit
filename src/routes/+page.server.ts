4f6b43f64516b9250b56e9d4edf467e1e4b75f49
Chia1104 <yuyuchia7423@gmail.com>
2023-09-13 03:59:35 +0800

    fix: change to ssr

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,23 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (() => {
-    const getServerRuntime = () => {
-        try {
-            return Bun.env.RUNTIME_ENV
-        } catch (e: any) {
-            return `node (ERROR: ${e.message})`
-        }
-    }
+export const load = (async () => {
+	const getPassword = async () => {
+		try {
+			const password = 'super-secure-pa$$word';
+			return await Bun.password.hash(password);
+		} catch (e: any) {
+			return `ERROR: ${e.message}`;
+		}
+	};
+	const getServerRuntime = () => {
+		try {
+			return Bun.env.RUNTIME_ENV;
+		} catch (e: any) {
+			return `node (ERROR: ${e.message})`;
+		}
+	};
 	return {
-		serverRuntime: getServerRuntime()
+		serverRuntime: getServerRuntime(),
+		password: await getPassword()
 	};
 }) satisfies PageServerLoad;
